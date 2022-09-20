fe0c7834b929de5dc890ae206ae2f2c98395f0fb
ainsleysm <smithainsley21@gmail.com>
2022-09-20 14:31:21 -0400

    Initial commit

---
+++ src/CrossProductIf.java
@@ -0,0 +1,7 @@
+public class CrossProductIf
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int age = 16;
+
+        if (age >= 16)
+        {
+            System.out.println("You can get a Drivers License");
+        }
+        else
+        {
+            System.out.println("You are too young for a Drivers License");
+        }
+
+        String playerA = "R";
+
+        if(playerA.equals("R") )
+        {
+            System.out.println("Move is Rock");
+        }
+        else if(playerA.equals("P") )
+        {
+            System.out.println("Move is Paper");
+        }
+        else
+        {
+            System.out.println("Has to be Scissors");
+        }
+
+        //String name = "Tom";
+        //String fname = name;
+    }
+}
